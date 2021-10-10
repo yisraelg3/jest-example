7219681e87567d92d7a1e2153a6a3db3c887244f
Yisrael Gurkow <yisrelg3@gmail.com>
2021-10-09 22:28:13 -0400

    tests written

--- tests/exampleFunctions.test.js
+++ tests/exampleFunctions.test.js
@@ -1,1 +1,34 @@
 const {getTotalLikes, getMostPopularBlog} = require("exampleFunctions")
+
+const user = {
+    username: "user1",
+    blogs:[
+        {
+            title: "Entry 1",
+            likes: 130,
+            content: "Blog 1 content..."
+        },
+        {
+            title: "Entry 2",
+            likes: 100,
+            content: "Blog 2 content..."
+        }
+    ]
+}
+
+describe('getTotalLikes', () => {
+    test('should return the total likes of a user'), () => {
+        expect( getTotalLikes(user) ).toBe(230)
+    }
+})
+
+describe('getMostPopularBlog', () => {
+    test('should return the most popular blog of a user'), () => {
+        const output = {
+            title: 'Entry 1',
+            likes: 130,
+            content: 'Blog 1 content...'
+        }
+        expect( getMostPopularBlog(user) ).toEqual(output)
+    }
+})
